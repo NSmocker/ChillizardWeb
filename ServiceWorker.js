f998d897d0a6577e27cb38644623633c7cfd2c53
NSmocker <32492163+NSmocker@users.noreply.github.com>
2025-01-08 13:33:38 +0200

    Fullscreen

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "SilverEchoGames-Chillizard the Chameleon-0.1.0";
+const contentToCache = [
+    "Build/ChillizardWeb.loader.js",
+    "Build/ChillizardWeb.framework.js.unityweb",
+    "Build/ChillizardWeb.data.unityweb",
+    "Build/ChillizardWeb.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
